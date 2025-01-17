1b6ba3f3c4c6212038c54a118554149672ccae62
Ola Adisa <olaoluhimself@yahoo.com>
2025-01-16 19:03:42 -0600

    Added .env file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 //SIMPLE CRUD APPLICATION - NO DATABASE
 
